3e234a56db491d1aa92fcbfd8fe3ed9311f3fc2e
Rudr Thakur <rudrakshacmkt777@gmail.com>
2022-05-11 17:12:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ app.get("/", (req, res) => res.send("Agora Auth Token Server"));
 
 
 app.post("/rtctoken", (req, res) => {
-    const appID = "cb25d30c28cc4ad081817a9325a4a576";
-    const appCertificate = "c81ad9e1590a402bad2d1ff697f93e6f";
+    const appID = "xxxxxxxxxxxxxxxxxxxx";
+    const appCertificate = "xxxxxxxxxxxxxxxxxxxxx";
     const expirationTimeInSeconds = 3600;
     const uid = Math.floor(Math.random() * 100000);
     const role = req.body.isPublisher ? Agora.RtcRole.PUBLISHER : Agora.RtcRole.SUBSCRIBER;
